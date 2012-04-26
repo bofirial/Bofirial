0954451e50dffba2e37421f73088db3e5c65f565
ide user ide_bofirial <james@jschafer.net>
2012-04-26 21:22:37 +0000

    NowJs module initial commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var colorThemes = require('./init/colorsInit.js').init(),
 	dal = require('./DAL/DAL.js'),
 	expressInit = require('./init/expressInit.js'),
 	app = expressInit.init(dal),
-	io = require('./init/socketioInit.js').init(app, expressInit.sessionStore),
+	everyone = require('./init/nowInit.js').init(app, expressInit.sessionStore),
 	types = require('./configs/types.js');
 	
 //Globals for all Views
@@ -53,30 +53,34 @@ app.post('/newgame', function(req, res) {
 	}});
 });
 
-io.sockets.on('connection', function (socket) {
-	var session = socket.handshake.session;
+everyone.now.logStuff = function(msg){
+    console.log(msg);
+};
 
-	socket.emit("init");
-
-	socket.on('Join General Chat', function(data) {
-		if (session.auth) {
-			socket.broadcast.to("General Chat").emit("New User", {name: session.user.firstName});
-		}
-		
-		socket.join("General Chat");
-	});
-	
-	socket.on("General Chat Message", function(data) {
-	
-		socket.broadcast.to("General Chat").emit("Message", {
-			name: session.user.firstName,
-			message: data.message
-		});
-	});
-	
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+//io.sockets.on('connection', function (socket) {
+//	var session = socket.handshake.session;
+//
+//	socket.emit("init");
+//
+//	socket.on('Join General Chat', function(data) {
+//		if (session.auth) {
+//			socket.broadcast.to("General Chat").emit("New User", {name: session.user.firstName});
+//		}
+//		
+//		socket.join("General Chat");
+//	});
+//	
+//	socket.on("General Chat Message", function(data) {
+//	
+//		socket.broadcast.to("General Chat").emit("Message", {
+//			name: session.user.firstName,
+//			message: data.message
+//		});
+//	});
+//	
+//  socket.on('my other event', function (data) {
+//    console.log(data);
+//  });
+//});
 
 app.listen(process.env.PORT);
